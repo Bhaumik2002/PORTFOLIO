e773e9e805fb3e0f9d5c8533dbb688d58c427422
Bhaumikj2002 <200303108102@paruluniversity.ac.in>
2023-07-30 22:25:26 +0530

    changes some nav section

--- script.js
+++ script.js
@@ -57,3 +57,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
+
+  menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
